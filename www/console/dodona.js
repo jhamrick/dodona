e44951911dfc8f7e16545c1df60e2694ec6321f1
Andrew M. Farrell <afarrell@Ender.(none)>
2009-04-11 22:23:31 -0400

    failed to connect web with dodona yet.

--- www/console/dodona.js
+++
@@ -1,9 +0,0 @@
-$(function onready(){
-$('#sendbutton').click(function(){
-var input = $('#chatinput').val();
-$.get('handler.php?msg='+input,
-	function(data){
-	    $('#chatbox').text($('#chatbox').text()+'> '+input+'\n'+data+'\n');
-      },'text');
-});
-});
