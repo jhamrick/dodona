e462a37bc3ed57310a4f2fd143050c1082ea73ae
Ashley Nichole Davis <davis@Waka.(none)>
2009-05-11 14:25:19 -0400

    Reworked questions some more, need to find a way to make it more recursive.

--- nlp.py
+++ nlp.py
@@ -34,20 +34,38 @@ def find_topic(parse, type=None):
     if type == None: type = get_sentence_type(parse)
     if isinstance(parse, str): return None
     #print parse
-    #tree = parse.productions()[0]
-    tree = parse[0]
+    tree = parse.productions()[0]
 
 
     if type == QUESTION:
+        tree = parse[0]
         
         for i in xrange(len(tree)):
             if tree[i].node == "Interrog_Clause":
                 for j in xrange(len(tree[i])):
+                    print tree[i][j].node
                     if tree[i][j].node == "Passive_Interrog_In":
                         for k in xrange(len(tree[i][j])):
-                           if re.match("^NP_.*$", tree[i][j][k].node)
+                           if re.match("^NP_.*$", tree[i][j][k].node):
+                                return tree[i][j][k].leaves()[0]
+                    if tree[i][j].node == "Passive_Interrog_Tr":
+                        
+                        for k in xrange(len(tree[i][j])):
+                           if re.match("^NP_.*$", tree[i][j][k].node):
                                 return tree[i][j][k].leaves()[0]
 
+            if tree[i].node == "Ind_Clause_Ques_Aux":
+                for j in xrange(len(tree[i])):
+                    if tree[i][j].node == "Ind_Clause_Inf":
+                        
+                        for k in xrange(len(tree[i][j])):
+                           if re.match("^NP_.*$", tree[i][j][k].node):
+                                return tree[i][j][k].leaves()[0]
+                    if tree[i][j].node == "Passive_Interrog_Tr":
+                        print "here"
+                        for k in xrange(len(tree[i][j])):
+                           if re.match("^NP_.*$", tree[i][j][k].node):
+                                return tree[i][j][k].leaves()[0]
 
     elif type == STATEMENT:
         pass
